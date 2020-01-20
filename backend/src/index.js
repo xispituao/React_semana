475d7890065e36dec44b3adbeb258e838944d619
Natanael Silva <natanso.s17@hotmail.com>
2020-01-20 09:54:38 -0300

    Finalizado web react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const cors = require('cors');
+const routes = require('./routes');
 const app = express();
 
 mongoose.connect('mongodb+srv://xispituao:NatanIfpi24@cluster0-m3k3g.mongodb.net/week10?retryWrites=true&w=majority', {
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://xispituao:NatanIfpi24@cluster0-m3k3g.mongodb.net
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,153 +1,45 @@
 import React, { useEffect, useState } from 'react'
+import api from './services/api'
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import DevForm from './components/DevForm'
+import DevItem from './components/DevItem'
 
 function App() {
-  const [github_username, setGithubUsername] = useState('')
-  const [techs, setTechs] = useState('')
+  const [devs, setDevs] = useState([]);
 
-  const [longitude, setLongitude] = useState('')
-  const [latitude, setLatitude] = useState('')
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      
+      setDevs(response.data);
+    }
 
-        setLatitude(latitude)
-        setLongitude(longitude)
-      },
-      (err) => {
-        console.log(err)
-      },
-      {
-        timeout: 3000,
-      }
-    )
-  }, [])
+    loadDevs();
+  }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault()
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', { data })
 
+    setDevs([...devs, response.data]);
   }
   
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github-username">Usuário do Github</label>
-            <input
-              name="github_ursename"
-              id="github-username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-       
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/28966511?s=460&v=4" alt="Natanael Silva"/>
-              <div className="user-info">
-                <strong>Natanael Silva</strong>
-                <span>Rails, Ruby, Django, Python</span>
-              </div>
-            </header>
-            <p>
-              Apaixonado pelas tecnologias e jogos.
-              <a href="https://github.com/xispituao">Acessar perfil no GitHub</a>
-            </p>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/28966511?s=460&v=4" alt="Natanael Silva"/>
-              <div className="user-info">
-                <strong>Natanael Silva</strong>
-                <span>Rails, Ruby, Django, Python</span>
-              </div>
-            </header>
-            <p>
-              Apaixonado pelas tecnologias e jogos.
-              <a href="https://github.com/xispituao">Acessar perfil no GitHub</a>
-            </p>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/28966511?s=460&v=4" alt="Natanael Silva"/>
-              <div className="user-info">
-                <strong>Natanael Silva</strong>
-                <span>Rails, Ruby, Django, Python</span>
-              </div>
-            </header>
-            <p>
-              Apaixonado pelas tecnologias e jogos.
-              <a href="https://github.com/xispituao">Acessar perfil no GitHub</a>
-            </p>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/28966511?s=460&v=4" alt="Natanael Silva"/>
-              <div className="user-info">
-                <strong>Natanael Silva</strong>
-                <span>Rails, Ruby, Django, Python</span>
-              </div>
-            </header>
-            <p>
-              Apaixonado pelas tecnologias e jogos.
-              <a href="https://github.com/xispituao">Acessar perfil no GitHub</a>
-            </p>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
